60b7ba8dbb660a31c9c30a26b72aa66ed02766c3
Xavier Arques <xavierarques@yahoo.com>
2017-11-21 15:47:09 +0100

    rails-task-manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "tasks", to: "tasks#index", as: :index
+  # get "tasks/new", to: "tasks#new", as: :new
+  # get "tasks/:id", to: "tasks#show", as: :show
+  # post "tasks", to: "tasks#create", as: :tasks
+
 end
